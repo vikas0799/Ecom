f77b8af3710f0fdbc38408b92bf8f6ea591fe653
Vikas Patel <vikaspatel07071999@gmail.com>
2024-04-16 22:39:33 +0530

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
       {/* <h1>welome to g30</h1> */}
-      <h1>welcome to chitkara</h1>
+      <h1>welcome to chitkara university</h1>
       <User/>
     </>
   )
